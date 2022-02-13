fdde6a17929095c323be7ebad12ca3ea55016bf9
Danil Fisiuk <fisyuklol@gmail.com>
2022-02-13 20:58:32 +0300

    SimpleCaching implementation

---
+++ Caching/CachingAbstract.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+
+template <class Key, class value_type>
+struct CachingAbstract
+{
+    void setMaxCacheSize(const std::size_t limit) { maxCacheSize = limit; }
+
+    virtual void putItem([[maybe_unused]] const Key&,
+                         [[maybe_unused]] const std::shared_ptr<value_type>&) = 0;
+
+    virtual std::shared_ptr<value_type> getItem(const Key& key) const = 0;
+
+    ~CachingAbstract() = default;
+
+protected:
+    std::unordered_map<Key, value_type> cacheItems;
+    std::size_t                         maxCacheSize = 2;
+    mutable std::mutex                  mut;
+};
---
+++ Caching/SimpleCache.hpp
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include <deque>
+#include <random>
+
+#include "CachingAbstract.hpp"
+
+template <class Key, class value_type>
+struct SimpleCache : CachingAbstract<Key, value_type>
+{
+    SimpleCache() = default;
+    SimpleCache(const size_t limit) { setMaxCacheSize(limit); }
+
+    void putItem(const Key& key, const std::shared_ptr<value_type>& valuePtr) override;
+    std::shared_ptr<value_type> getItem(const Key& key) const override;
+};
+
+template <class Key, class value_type>
+void SimpleCache<Key, value_type>::putItem(const Key&                         key,
+                                           const std::shared_ptr<value_type>& valuePtr)
+{
+    std::lock_guard<std::mutex> lg(mut);
+
+    auto mapItemIterator = cacheItems.find(key);
+    if (mapItemIterator != cacheItems.end())
+    {
+        mapItemIterator->second = *valuePtr;
+        return;
+    }
+
+    if (cacheItems.size() + 1 > maxCacheSize)
+    {
+        cacheItems.erase(cacheItems.begin());
+    }
+
+    cacheItems[key] = *valuePtr;
+}
+
+template <class Key, class value_type>
+std::shared_ptr<value_type> SimpleCache<Key, value_type>::getItem(const Key& key) const
+{
+    std::lock_guard<std::mutex> lg(mut);
+
+    const auto mapItemIterator = cacheItems.find(key);
+    if (mapItemIterator == cacheItems.cend())
+    {
+        throw std::range_error("No such key in the cache");
+    }
+
+    return std::make_shared<value_type>(mapItemIterator->second);
+}
