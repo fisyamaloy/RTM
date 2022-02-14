2e09aeddeede3ee7d13497d416a03cf9ffa8af1c
Danil Fisiuk <fisyuklol@gmail.com>
2022-02-14 22:28:34 +0300

    structure changes

---
+++ Standart_17/Examples.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "Examples/FoldExpression.hpp"
+#include "Examples/StringView.hpp"
+#include "Examples/FileSystem.hpp"
+#include "Examples/OptionaL.hpp"
+#include "Examples/AnY.hpp"
+#include "Examples/VarianT.hpp"
--- Standart_17/main.cpp
+++ Standart_17/main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-
-#include "Examples/FoldExpression.hpp"
-#include "Examples/StringView.hpp"
-#include "Examples/FileSystem.hpp"
-#include "Examples/OptionaL.hpp"
-#include "Examples/AnY.hpp"
-#include "Examples/VarianT.hpp"
+#include "Examples.hpp"
 
 int main()
 {
